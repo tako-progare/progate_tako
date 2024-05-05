c5f58deb7422157844dd7c6ff3a218166f0bf3ea
oyu <12345yuu6789@gmail.com>
2024-05-05 13:48:18 +0900

    wip

--- public/js/GetGoal.js
+++ public/js/GetGoal.js
@@ -4,15 +4,14 @@ export function GetGoal() {
       userid: localStorage.getItem('userID'),
     }
     const query_params = new URLSearchParams(params);
-
+    console.log(params);
     return fetch('http://localhost:4000/goal_location?' + query_params)
       .then(response => response.json())
       .then(response => {
+        console.log("response:",response);
         // サーバーから取得した位置情報のうち、緯度と経度のみを取得
         const latitude = response[0].lat_goal; // 配列の0番目の要素から緯度を取得
-        const longitude = response[0].lon_goal; // 配列の0番目の要素から経度を取得
-
-        console.log("response:",response);
+        const longitude = response[0].lon_goal; // 配列の0番目の要素から経度を取得   
 
         return { latitude, longitude }; // 緯度と経度の情報のみを返す
       })
--- public/play.js
+++ public/play.js
@@ -1,4 +1,62 @@
-//GUESS button 押された時
-window.clickGuess = () => {
-  window.location.href="./result.html";
+let map, infoWindow;
+
+let destinationRange = 100;
+
+//initMap 呼び出し
+//mapを作成する
+window.initMap = initMap;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 35.68224057589321, lng: 139.76728396076678 },
+    zoom: 15,
+    zoomControl: false,
+    mapTypeControl: false,
+    streetViewControl: false,
+  });
+
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+
+      //マーカーを作成して地図上に表示
+      const marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        title: "終了地点",
+      });
+
+      // マーカーがクリックされたときの情報ウィンドウを設定
+      marker.addListener("click", () => {
+        infoWindow.setContent("終了地点");
+        infoWindow.open(map, marker);
+      });
+
+      map.setCenter(pos);
+    },
+    () => {
+      handleLocationError(true, infoWindow, map.getCenter());
+    }
+  );
 }
+
+if (window.addEventListener) {
+    window.addEventListener('load', onLoad);
+  } else {
+    // もし `addEventListener` がサポートされていない場合、古いブラウザの場合に適切に動作するように代替処理を行う
+    window.attachEvent('onload', onLoad);
+  }
+  
+  function onLoad() {
+    const guess = document.getElementById("guess");
+    guess.addEventListener("click", Play);
+    infoWindow = new google.maps.InfoWindow();
+  
+  }
+  function Play(){
+    window.location.href="./result.html";
+  }
+  
